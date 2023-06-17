bc3b131cb333755e9a6734acab1a619cd80fb161
Sergio Espinoza <sergiojose180601@gmail.com>
2023-06-17 06:51:22 +0000

    paragraph types, pattern types and basic page

---
+++ project/web/sites/default/files/php/twig/648d57714c9f3_pattern-text.html.twig_yg_R4dFHTQgvaN3m5uEZ1I790/neQgjEyyTyfh4tMmWFjr6fOvU-POVEDA_P6C2IbnBpI.php
@@ -0,0 +1,88 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/lab03/patterns/attoms/card_title/pattern-text.html.twig */
+class __TwigTemplate_d8fc58a545a5bef70fa343af14299e5a extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<p class=\"pstyle\">¿Sabía que...?</p>
+<p class=\"pstyle\">Para poder ejercer y realizar sus tareas y funciones<br>
+como notaria o notario público, debe mantener<br>
+actualizados sus datos, tales como teléfonos<br>
+direcciones y correo electrónico </p>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/lab03/patterns/attoms/card_title/pattern-text.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/lab03/patterns/attoms/card_title/pattern-text.html.twig", "/var/www/html/web/themes/lab03/patterns/attoms/card_title/pattern-text.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ project/web/themes/lab03/js/lab03.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * lab03 behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.lab03 = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
